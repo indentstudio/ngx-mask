30481d90797b84b8fe78326dfb938a8668522a57
raszpi <raszpii@gmail.com>
2022-10-24 12:55:36 +0200

    remove everything

--- projects/ngx-mask-lib/cypress/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import { startAngularDevServer } from '@jscutlery/cypress-angular';
-
-// @ts-ignore
-module.exports = (on, config) => {
-	on('dev-server:start', (options: any) =>
-		startAngularDevServer({ options, tsConfig: 'tsconfig.cypress.json' }),
-	);
-	return config;
-};
--- src/app/bugs/shadow-dom/shadow-dom.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-	selector: 'mask-shadow-dom',
-	templateUrl: './shadow-dom.component.html',
-	styleUrls: ['./shadow-dom.component.scss'],
-	encapsulation: ViewEncapsulation.ShadowDom,
-})
-export class ShadowDomComponent {}
